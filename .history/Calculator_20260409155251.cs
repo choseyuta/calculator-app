0911c50b36837714d31d25e9086d92dbcc2affe8
choseyuta <y.prsetya@students.amikom.ac.id>
2026-04-09 15:53:31 +0700

    menambahkan fungsi penambahan

--- .history/Calculator_20260409155251.cs
+++ .history/Calculator_20260409155251.cs
@@ -5,6 +5,10 @@ class Calculator
         // Tugas 2: Menambahkan fungsi Penambahan
         return a + b;
     }
-    
+    static int Pengurangan(int a, int b)
+    {
+        // Tugas 3: Menambahkan fungsi Pengurangan
+        return a - b;
+    }
 }
 
